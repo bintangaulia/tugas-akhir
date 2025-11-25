ce874ee1c46f7c0e07799bf0682c3a6fd543ace7
Muhammad Rizky Al Aziz <23102023@student.itsk-soepraoen.ac.id>
2025-11-25 18:03:20 +0700

    push-2

--- backend/resources/views/home.blade.php
+++ backend/resources/views/home.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
 <body class="bg-light">
+    
     <div class="container py-4">
+        
         <h1 class="text-center fw-bold position-relative title-decor display-6">Konten</h1>
         <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, in?</p>
         <div class="d-flex gap-2 mb-4 text-center justify-content-center">
@@ -126,6 +128,25 @@
                 </div>
             </div>
         </div>     
+        <div class="col-md-3">
+                <div class="card h-100 shadow-sm border-0 rounded-3">
+                    <img class="border-0 rounded-2" src="{{ asset('template') }}/assets/img/le.jpg" alt="" width="100%">
+                    <div class="card-body">
+                        <span class="badge bg-primary w-100 py-2 mb-3">Artikel</span>
+                        <div class="d-flex align-items-center text-muted small mb-2">
+                            <span class="me-3">👤 Administrator</span>
+                            <span>📅 06 April, 2022</span>
+                        </div>
+                        <h5 class="fw-bold">Judul Artikel</h5>
+                        <p class="text-muted small text-justify">
+                        Lorem ipsum dolor sit amet consect adipisicing elit. Necessitatibus omnis voluptas hic officiis esse ducimus?
+                    </p>
+                    <a href="#" class="text-primary text-decoration-none fw-semibold text-right">
+                        BACA SELENGKAPNYA
+                    </a>
+                </div>
+            </div>
+        </div> 
         <div class="col-md-3">
                 <div class="card h-100 shadow-sm border-0 rounded-3">
                     <img class="border-0 rounded-2" src="{{ asset('template') }}/assets/img/le.jpg" alt="" width="100%">
